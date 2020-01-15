2460507a1e045b795c467c64aec84118eb0ea96a
Kevin <knphillips01@gmail.com>
2020-01-15 16:40:58 -0600

    created .map and html createElements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,21 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+const articles = document.querySelector('.articles');
+
+data.forEach((obj) => {
+  articles.appendChild(create(obj));
+});
+
+function create(obj) {
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const date = document.createElement('p');
+  const paragraphOne = document.createElement('p');
+  const paragraphTwo = document.createElement('p');
+  const paragraphThree = document.createElement('p');
+  const button = document.createElement('span');
+
+
+}
